fb9a1ccc9ef2db25578ef1028a3b512a5c5ada6a
netzgiest <netzgiest@gmail.co>
2024-02-13 22:32:46 +0300

    fix head

--- export_v2.py
+++ export_v2.py
@@ -3,8 +3,9 @@ import xml.etree.ElementTree as ET
 from datetime import datetime
 import json,os,pytz, uuid,zipfile
 import tkinter as tk
-from tkinter import filedialog
 import tkinter.messagebox as messagebox
+cashplan_namespace = "http://mil.ru/cashPlan"
+ET.register_namespace('',cashplan_namespace)
 
 def load_config(filename):
     with open(filename, "r", encoding='utf-8') as configfile:
@@ -16,10 +17,11 @@ def current_datetime(value=None):
     tz = pytz.timezone("Europe/Moscow")
     if value is None:
      now_utc = datetime.now(pytz.UTC)
-     now_local = now_utc.astimezone(tz)
+     now_local = now_utc.astimezone(tz).isoformat()
     else:
-     now_local= tz.localize(datetime.strptime(str(value), "%d.%m.%Y")).replace(hour=0, minute=0, second=0)  
-    return now_local.isoformat()
+     now_local= tz.localize(datetime.strptime(str(value), "%d.%m.%Y")).replace(hour=0, minute=0, second=0).isoformat(timespec='seconds')
+      
+    return now_local
 
 def load_workbook_data(filename):
     wb = load_workbook(filename=filename, read_only=True)
@@ -39,7 +41,7 @@ def prettify_xml(element, level=0, indent='   '):
 
 def xml_to_pretty_string(root):
     prettify_xml(root)
-    return ET.tostring(root, encoding='unicode')
+    return ET.tostring(root, encoding='utf-8',xml_declaration=True,method='xml')
 
 def add_to_zip(filepath,INN,UID):
     zip_name = os.path.dirname(filepath) + f"{INN}_{datetime.now().strftime('%Y%m%d')}_{UID}.zip"
@@ -66,27 +68,40 @@ def find_first_row_with_prefix(sheet, start_row, prefix):
     return None
 def create_xml(sheet1,sheet2, config, now_local):
     thisUID = str(uuid.uuid4())
-    root =ET.Element('Message',{'CreateDate':str(now_local),'UID':thisUID,'PreviousUID':str(config["PreviousUID"])})
+
+    root =ET.Element("Message")
+    root.set('CreateDate',str(now_local))
+    root.set('UID',str(thisUID))
+    root.set('PreviousUID',str(config["PreviousUID"]))
     config["PreviousUID"] = thisUID
-    Organization =ET.SubElement(root, "Organization",{'INN':str(config["INN"]),"KPP":config["KPP"],'Name':config["NAME"],
-                                                     "GOZUID":str(sheet1[config["start_indexes"]["GOZUID"]].value),
-                                                     "ContractDate":str(current_datetime(sheet1[config["start_indexes"]["ContractDate"]].value)) 
-                                                     })
-    Forms = ET.SubElement(root,"Forms")
+    Organization =ET.SubElement(root, "Organization")
+    Organization.set('INN',str(config["INN"]))
+    Organization.set("KPP",config["KPP"])
+    Organization.set('Name',config["NAME"])
+    Organization.set("GOZUID",str(sheet1[config["start_indexes"]["GOZUID"]].value))
+    Organization.set("ContractDate",str(current_datetime(sheet1[config["start_indexes"]["ContractDate"]].value))) 
+                                                 
+    Forms=ET.SubElement(root,"Forms")
     Form8=ET.SubElement(Forms,"Form",{'type':'8',"ReportDate":str(now_local),'Year':str(sheet1[config['start_indexes']['Year']].value),
                                       'Quarter':str(sheet1[config["start_indexes"]["Ouarter"]].value)})
-    ContractSpending=ET.SubElement(Form8,"ContractSpending",{"Salary":str(int(sheet1[config["start_indexes"]["Salary"]].value*100)),
-                                                             "Taxes":str(int(sheet1[config["start_indexes"]["Taxes"]].value*100)),
-                                                             "Rates":str(int(sheet1[config["start_indexes"]["Rates"]].value*100)),
-                                                             "Other":str(int(sheet1[config["start_indexes"]["Other"]].value*100)),
-                                                             "Reserve":str(int(sheet1[config["start_indexes"]["Reserve"]].value*100)),
-                                                             "Income":str(int(sheet1[config["start_indexes"]["Income"]].value)*100)
-                                                             })
-    Contractors = ET.SubElement(Form8, 'Contractors') 
-    #int(config["start_indexes"]["startcontractor"])
-    for row in range(find_first_row_with_prefix(sheet1,1,"2."), sheet1.max_row + 1):
-     if str(sheet1.cell(row=row, column=1).value).startswith('2'):
-      Contractor = ET.SubElement(Contractors,"Contractor",{'Total':str(int(sheet1.cell(row=row, column=3).value)*100),
+    ContractSpending=ET.SubElement(Form8,"ContractSpending")
+    Contractors = ET.SubElement(Form8, 'Contractors')
+    ContractFinance=ET.SubElement(Form8,"ContractFinance")
+    for row in range(find_first_row_with_prefix(sheet1,1,"1."), sheet1.max_row + 1):
+      if str(sheet1.cell(row=row, column=1).value).startswith('1.1'):  
+        ContractSpending.set("Salary",str(int(sheet1.cell(row=row, column=3).value*100)))
+      if str(sheet1.cell(row=row, column=1).value).startswith('1.2'):  
+        ContractSpending.set("Taxes",str(int(sheet1.cell(row=row, column=3).value*100)))
+      if str(sheet1.cell(row=row, column=1).value).startswith('1.3'):   
+        ContractSpending.set("Rates",str(int(sheet1.cell(row=row, column=3).value*100)))
+      if str(sheet1.cell(row=row, column=1).value).startswith('1.4'):    
+        ContractSpending.set("Other",str(int(sheet1.cell(row=row, column=3).value*100)))
+      if str(sheet1.cell(row=row, column=1).value).startswith('1.5'):  
+        ContractSpending.set("Reserve",str(int(sheet1.cell(row=row, column=3).value*100)))
+      if str(sheet1.cell(row=row, column=1).value).startswith('1.6'):  
+        ContractSpending.set("Income",str(int(sheet1.cell(row=row, column=3).value*100)))
+      if str(sheet1.cell(row=row, column=1).value).startswith('2.'):
+       Contractor = ET.SubElement(Contractors,"Contractor",{'Total':str(int(sheet1.cell(row=row, column=3).value)*100),
                                                            'Name':str(sheet1.cell(row=row, column=4).value),
                                                            'INN':str(sheet1.cell(row=row, column=5).value),
                                                            'ContractNumber':str(sheet1.cell(row=row, column=6).value),
@@ -96,14 +111,20 @@ def create_xml(sheet1,sheet2, config, now_local):
                                                            'PaymentPlanned':str(int(sheet1.cell(row=row, column=10).value)*100),
                                                            'FinishDate': str(current_datetime(sheet1.cell(row=row, column=12).value))})
         
-    nextCell=find_last_row_with_prefix(sheet1,1,'2')
-    PlannedPay=ET.SubElement(Form8,"PlannedPay",{"Total":str(int(sheet1['I'+str(nextCell)].value)*100),
-                                                 "PaymentPlanned":str(int(sheet1['J'+str(nextCell)].value)*100),
-                                                 "PaymentCurrent":str(int(sheet1['C'+str(nextCell)].value)*100)})
-    ContractFinance=ET.SubElement(Form8,"ContractFinance",{"TotalRequirement":str(int(sheet1['C'+str(nextCell+1)].value)*100),
-                                                           "CashBalance":str(int(sheet1['C'+str(nextCell+2)].value)*100),
-                                                           "PlannedIncome":str(int(sheet1['C'+str(nextCell+3)].value)*100),
-                                                           "DepositeIncome":str(int(sheet1['C'+str(nextCell+4)].value)*100)})
+    #nextCell=find_last_row_with_prefix(sheet1,1,'2')
+    if str(sheet1.cell(row=row, column=1).value).startswith('3'):
+     PlannedPay=ET.SubElement(Form8,"PlannedPay",{"Total":str(int(sheet1.cell(row=row, column =9).value)*100), #цена договора?
+                                                 "PaymentPlanned":str(int(sheet1(row=row, column =10).value)*100),
+                                                 "PaymentCurrent":str(int(sheet1(row=row, column =3).value)*100)})
+    if str(sheet1.cell(row=row, column=1).value).startswith('4'):
+     ContractFinance.set("TotalRequirement",str(int(sheet1.cell(row=row, column=3).value*100)))
+    if str(sheet1.cell(row=row, column=1).value).startswith('5'): 
+     ContractFinance.set("CashBalance",str(int(sheet1.cell(row=row, column=3).value)*100))
+    if str(sheet1.cell(row=row, column=1).value).startswith('6'):
+     ContractFinance.set("PlannedIncome",str(int(sheet1.cell(row=row, column=3).value)*100))
+    if str(sheet1.cell(row=row, column=1).value).startswith('7'):
+     ContractFinance.set("DepositeIncome",str(int(sheet1.cell(row=row, column=3).value)*100))
+    
     FormSupplement=ET.SubElement(Forms,"Form",{'type':'Supplement',"ReportDate":str(now_local)})
     Parts=ET.SubElement(FormSupplement,"Parts")
    # хз будем считать что с 3 строки и год начинается с 202 и данные начинаются с 1 ячейки
@@ -121,6 +142,7 @@ def create_xml(sheet1,sheet2, config, now_local):
          if  reason.strip() != 'None' and reason.strip() != '':
             ET.SubElement(Reasons,"Reason").text=reason.strip()
     xmlstr = xml_to_pretty_string(root)
+    
     return xmlstr,config
 
 def update_config(filename, config):
@@ -129,12 +151,14 @@ def update_config(filename, config):
         configfile.write(config_json)
 
 def save_xml(xmlstr, filename):
-    with open(filename, "w",encoding='utf-8') as f:
+    with open(filename, "wb") as f:
         f.write(xmlstr)
 
 def main():
     root = tk.Tk()
     root.withdraw()
+    
+    from tkinter import filedialog
     xlsx_file_path = filedialog.askopenfilename(title="Выберите XLSX файл", filetypes=[("XLSX Files", "*.xlsx")])
     config = load_config("config.json")
     workbook = load_workbook_data(xlsx_file_path)
